cbbeb7fd91c428dbee9e89337c6129d84c44286a
cc231008 <cc231008@fhstp.ac.at>
2024-06-10 18:02:54 +0200

    Last Changes

---
+++ client/my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <header>
+          <h1>My React App</h1>
+        </header>
+        <main>
+          <p>Welcome to my React app!</p>
+        </main>
+      </div>
+  );
+}
+
+export default App;
